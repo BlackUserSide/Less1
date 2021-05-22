0e31080c0b2c2b0679ea57770d8a68d6ae2c598e
BlackUserSide <nesmserg@gmail.com>
2021-05-22 22:02:22 +0300

    fix_2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 document.getElementById('hamburger').onclick = () => {
     let el = document.getElementById('hamburger')
     el.classList.toggle('is-active')
-    
+    let el1 = document.getElementById('nav')
+    el1.classList.toggle('list-active')
 }
